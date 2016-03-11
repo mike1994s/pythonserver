60f303bd1dffa2231275de587ebed79d93a27e23
Misha <meshuin94@gmail.com>
2016-03-11 16:39:24 +0400

    task 1

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+BASE_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -80,3 +80,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+TEMPLATE_DIRS = (
+	BASE_DIR + '/templates',
+)
+STATIC_ROOT = BASE_DIR + '/static'
--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -5,8 +5,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'qa.views.test'),
+    url(r'^login/', 'qa.views.test'),
+    url(r'^signup/', 'qa.views.test'),
+    url(r'^question/(\d+)/$', 'qa.views.test'),
+    url(r'^ask/$', 'qa.views.test'),
+    url(r'^popular/', 'qa.views.test'),
+    url(r'^new/$', 'qa.views.test'),  
 )
