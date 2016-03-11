1c576a74cf376bbf7332db4168acef06efad19d0
Your Name <you@example.com>
2016-03-11 12:58:17 +0000

    fix

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -6,10 +6,10 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'qa.views.test'),
-    url(r'^login/', 'qa.views.test'),
-    url(r'^signup/', 'qa.views.test'),
-    url(r'^question/(\d+)/$', 'qa.views.test'),
-    url(r'^ask/$', 'qa.views.test'),
-    url(r'^popular/', 'qa.views.test'),
-    url(r'^new/$', 'qa.views.test'),  
+    url(r'^login/.*', 'qa.views.test'),
+    url(r'^signup/.*', 'qa.views.test'),
+    url(r'^question/\d+/', 'qa.views.test'),
+    url(r'^ask/.*', 'qa.views.test'),
+    url(r'^popular/.*', 'qa.views.test'),
+    url(r'^new/.*', 'qa.views.test'),  
 )
