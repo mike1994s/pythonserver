f91fbd66591a4bad1e1fb5bb3e48b4b027c4b340
Misha <meshuin94@gmail.com>
2016-03-12 18:37:50 +0400

    db working

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+   
+     'qa'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -56,9 +58,17 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # 'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ #   }
+      'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'ask',
+        'USER': 'root',
+        'PASSWORD': '123123',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Question(models.Model):
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateField()
+	rating = models.IntegerField()
+	author = models.OneToOneField(User)
+	likes  = models.ForeignKey(User,related_name='likes', null=True, on_delete=models.CASCADE)
+	def __unicode__(self):
+		return self.title
+	class Meta:
+		ordering = ['-added_at']
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateField()
+	question = models.OneToOneField(Question,related_name='question')
+	author = models.OneToOneField(User)
+	def __unicode__(self):
+		return self.text
+	class Meta:
+		ordering = ['-added_at']
